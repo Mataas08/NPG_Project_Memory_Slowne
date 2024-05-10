75638dbe67b4959361625c0809d6126cc78a0184
Mataas <55620999+Mataas08@users.noreply.github.com>
2024-05-10 12:02:21 +0200

    Add files via upload

---
+++ test/js/game.js
@@ -0,0 +1,81 @@
+var wyniki = [0,0,0,0,0,0,0];
+var attempt = 0;
+
+var textRem;
+var round = 0;
+var words = [];
+
+function startGame(){
+	var mode = document.getElementById("easyModeSelect").checked;
+	if(mode == true){
+		words = [...short_words];
+    }else{
+    	words = [...long_words];
+    }
+    game();
+}
+
+function backToLobby() {
+    changeVisibilityOfDisplay("start");
+    round = 0;
+    textRem = "";
+    attempt++;
+    showScoreBoard();
+}
+
+function game() {
+    round = round + 1;
+
+    if (round > words.length) {
+        backToLobby();
+        alert("XDD");
+    } else {
+        var actualText = [];
+        var wordsInRound = [];
+
+        for (let i = 0; i < round; i++) {
+            var textToAdd;
+
+            do {
+                textToAdd = words[getRandomInt(words.length)].trim();
+            } while (wordsInRound.includes(textToAdd));
+
+            wordsInRound.push(textToAdd);
+            actualText.push(textToAdd);
+        }
+
+        textRem = actualText.join(" ");
+
+        changeVisibilityOfDisplay("showText");
+        document.getElementById("showTextLabel").innerText = textRem;
+
+        countingDown(500, 6, "showTimeLabel");
+
+        sleep(3500).then(() => {
+            document.getElementById("showTimeLabel").innerText = "0";
+
+            changeVisibilityOfDisplay("enterText");
+        });
+    }
+}
+
+function submitAnswer() {
+    var x = document.getElementById("answer").value;
+    if (x == textRem) {
+        changeVisibilityOfDisplay("roundWon");
+
+        countingDown(500, 4, "roundWonLabel");
+
+        sleep(2500).then(() => {
+            changeVisibilityOfDisplay("enterText");
+            game();
+        });
+
+    } else {
+        changeVisibilityOfDisplay("score");
+        document.getElementById("showResultLabel").innerText = round - 1;
+        wyniki.push((round - 1).toString());
+
+        showScoreBoard();
+    }
+}
---
+++ test/js/scoreBoard.js
@@ -0,0 +1,46 @@
+function showScoreBoard() {
+    var x = [0, 0, 0, 0, 0, 0, 0];
+    var y = [0, 0, 0, 0, 0, 0, 0];
+
+    for (let i = 0; i < attempt; i++) {
+        x[6 - i] = attempt - i - 1;
+    }
+
+    var data = wyniki.join(" ").trim();
+    var values = data.split(' ').map(Number);
+    var y = values.slice(-7);
+
+    var chLine = document.getElementById("chLine");
+    var chartData = {
+        labels: [x[0], x[1], x[2], x[3], x[4], x[5], x[6]],
+        datasets: [{
+            data: [y[0], y[1], y[2], y[3], y[4], y[5], y[6]],
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+        }]
+    };
+
+    if (chLine) {
+        new Chart(chLine, {
+            type: 'line',
+            data: chartData,
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                responsive: true
+            }
+        });
+    }
+
+    document.getElementById("showResultLabel").innerText = round - 1;
+}
