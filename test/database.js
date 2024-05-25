4a65ee43db4fab10f14a3b2550bd3aa291471bf7
KacperMasterXD <163449505+KacperPolewka@users.noreply.github.com>
2024-05-25 10:27:06 +0200

    Dodanie mid_words do bazy.js

--- test/database.js
+++ test/database.js
@@ -1,5 +1,18 @@
 const short_words = [
-    "kotek", "jajko", "bluza", "kabel", "nóż", "łyżka", "kebab", "mysz", "kwiat", "okręt",
+	"klub", "rok", "sok", "bok", "krój", "strój", "bicz", "woda", "gad", "ssak",
+    "tren", "czas", "kok", "grab", "lin", "baza", "runy", "rak", "lew", "lot",
+    "gust", "fart", "żel", "krem", "kiść", "sen", "noc", "kit", "słój", "moc",
+    "foka", "mysz", "koń", "lek", "kleń", "hak", "stan", "plan", "krew", "żyła",
+    "tlen", "azot", "hel", "stal", "dysk", "test", "wór", "mors", "lew", "pan",
+    "pani", "kość", "maj", "luty", "post", "dar", "tors", "dron", "ręka", "ból",
+    "żaba", "lipa", "kort", "piła", "kosa", "tor", "ład", "mus", "haft", "klej",
+    "byk", "siła", "port", "sort", "bat", "ster", "fugi", "jaź", "płoć", "krąp",
+    "łódź", "wieś", "sos", "kij", "dzik", "tusz", "huk", "ryk", "buk", "grab",
+    "liść", "koło", "rama", "kler", "keks", "nos", "brak", "prąd", "takt", "nie"
+];
+
+const mid_words = [
+	"kotek", "jajko", "bluza", "kabel", "nóż", "łyżka", "kebab", "mysz", "kwiat", "okręt",
     "arbuz", "ser", "joga", "auto", "tir", "mleko", "kubek", "most", "pilot", "lampa",
     "dywan", "list", "waga", "dron", "łąka", "buty", "kufel", "klucz", "wózek", "koc",
     "okoń", "karp", "sum", "lina", "gęś", "gołąb", "dżem", "krowa", "rower", "ptak",
@@ -12,7 +25,7 @@ const short_words = [
 ];
 
 const long_words = [
-    "hipopotam", "aligator", "krokodyl", "niedźwiedź", "pantera", "dinozaur", "myszołów", "heloderma", "tygrys", "piżmowół",
+	"hipopotam", "aligator", "krokodyl", "niedźwiedź", "pantera", "dinozaur", "myszołów", "heloderma", "tygrys", "piżmowół",
     "ananas", "jogurt", "awokado", "durian", "gruszka", "pomarańcza", "banany", "cytryna", "pomidor", "marchew",
     "azbest", "podłoga", "poduszka", "kołdra", "mokradła", "karczma", "rozstaje", "gigant", "akademik", "miasto",
     "klajster", "książka", "komiks", "komputer", "laptop", "zeszyt", "piórnik", "słuchawki", "nauszniki", "czapla",
